dd73412fe5600347ec0fd9190da7ac7aa51444b1
Chelsea Isaacson <chelseaisaacson@Chelseas-MacBook-Air.local>
2020-04-09 17:06:06 -0700

    MVP

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,6 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
@@ -34,16 +35,46 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+room['outside'].add_item(Item('Sword', 'Cuts things'))
+room['outside'].add_item(Item('Rock', 'Breaks things'))
+room['overlook'].add_item(Item('Map', 'Tells you where to go'))
+room['narrow'].add_item(Item('Key', 'Opens things'))
+room['foyer'].add_item(Item('Potion', 'Renders things inactive'))
+room['treasure'].add_item(Item('Gold', 'You win with this'))
 #
 # Main
 #
+class colors:
+    blink = '\033[5m'
+    red = '\033[91m'
+    yellow = '\033[93m'
+    reset = '\033[0m'
+
+def invalidSelection(sentance = '\nYou cannot go that way!\n'):
+    print(f'{colors.red}{colors.blink}{sentance}{colors.reset}')
+
+def commands():
+    print('\nPlease choose a command...\n')
+    return input("[n] north  [s] south  [e] east [w] west [Get item] [Drop item] [i] Get inventory [q] Quit\n").lower()
+
+def playerInfo():
+    print(f"\n{colors.yellow}{name}, You are in the {player.current_room.name} \n {player.current_room.description}{colors.reset} \n")
+
 player = Player(room['outside'])
-print(f" You are currently in the {player.current_room.name}, \n {player.current_room.description}")
-command = input("[n] north  [s] south  [e] east [w] west [q] Quit\n")
+
+def item_func():
+    print('Items in room:')
+    for item in player.current_room.items:
+        print(f'{colors.yellow}{item.name} -- {item.description}{colors.reset}')
+
+name = input('What is your name?\n' )
+playerInfo()
+item_func()
+command = commands()
 
 # Make a new player object that is currently in the 'outside' room.
 
-player = Player(room['outside'])
+# player = Player(room['outside'])
 
 # Write a loop that:
 #
@@ -55,38 +86,87 @@ player = Player(room['outside'])
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
-while not command == 'q':
-    if player.current_room == room['outside']:
-        if command == 'n':
-            player.update_room(room['foyer'])
-        else:
-            print("You cannot go that way!")
-    elif player.current_room == room['foyer']:
-        if command == 'n':
-            player.update_room(room['overlook'])
-        elif command == 'e':
-            player.update_room(room['narrow'])
-        elif command == 's':
-            player.update_room(room['outside'])
-        else:
-            print("You cannot go that way!")
-    elif player.current_room == room['overlook']:
-        if command == 's':
-            player.update_room(room['foyer'])
-        else:
-            print("You cannot go that way!")
-    elif player.current_room == room['narrow']:
-        if command == 'w':
-            player.update_room(room['foyer'])
-        elif command == 'n':
-            player.update_room(room['treasure'])
-        else:
-            print('You cannot go this way!')
-    elif player.current_room == room['treasure']:
-        if command == 's':
-            player.update_room(room['narrow'])
-        else:
-            print('You cannot go that way!')
 
-    print(f" You are currently in the {player.current_room.name}, \n {player.current_room.description}")
-    command = input("[n] north  [s] south  [e] east [w] west [q] Quit\n")
+splitCommand = command.split(' ')
+
+while not command == 'q':
+    splitCommand = command.split(' ')
+    if len(splitCommand) == 1:
+        if command == 'i':
+            player.get_inventory()
+        elif command == 'n':
+            if player.current_room.n_to == None:
+                invalidSelection()
+            else:
+                player.update_room(player.current_room.n_to)
+        elif command == 's':
+            if player.current_room.s_to == None:
+                invalidSelection()
+            else: 
+                player.update_room(player.current_room.s_to)
+        elif command == 'w':
+            if player.current_room.w_to == None:
+                invalidSelection()
+            else: 
+                player.update_room(player.current_room.w_to)
+        elif command == 'e':
+            if player.current_room.e_to == None:
+                invalidSelection()
+            else:
+                player.update_room(player.current_room.e_to)
+        else:
+            invalidSelection('Try again, pick a valid direction this time')
+    elif len(splitCommand) == 2:
+        if splitCommand[0].lower() == 'get':
+            for item in player.current_room.items:
+                if item.name.lower() == splitCommand[1].lower():
+                    player.current_room.items.remove(item)
+                    player.inventory.append(item)
+                    item.on_take(item.name.lower())
+        elif splitCommand[0].lower() == 'drop':
+            for item in player.inventory:
+                if item.name.lower() == splitCommand[1].lower():
+                    player.inventory.remove(item)
+                    player.current_room.items.append(item)
+                    item.on_drop(item.name.lower())
+        else:
+            print('That command does not exist')
+    else:
+        print('Try another command')
+
+    playerInfo()
+    item_func()
+    command = commands()
+
+    # if player.current_room == room['outside']:
+    #     # print('Options: North to the Foyer')
+    #     if command == 'n':
+    #         player.update_room(room['foyer'])
+    #     else:
+    #         print("You cannot go that way!\n\n")
+    # elif player.current_room == room['foyer']:
+    #     if command == 'n':
+    #         player.update_room(room['overlook'])
+    #     elif command == 'e':
+    #         player.update_room(room['narrow'])
+    #     elif command == 's':
+    #         player.update_room(room['outside'])
+    #     else:
+    #         print("You cannot go that way!\n\n")
+    # elif player.current_room == room['overlook']:
+    #     if command == 's':
+    #         player.update_room(room['foyer'])
+    #     else:
+    #         print("You cannot go that way!")
+    # elif player.current_room == room['narrow']:
+    #     if command == 'w':
+    #         player.update_room(room['foyer'])
+    #     elif command == 'n':
+    #         player.update_room(room['treasure'])
+    #     else:
+    #         print('You cannot go this way!\n\n')
+    # elif player.current_room == room['treasure']:
+    #     if command == 's':
+    #         player.update_room(room['narrow'])
+    #     else:
+    #         print('You cannot go that way!\n\n')
--- src/player.py
+++ src/player.py
@@ -4,9 +4,13 @@ class Player():
     def __init__(self, current_room, name = 'Player 1', ):
         self.name = name
         self.current_room = current_room
+        self.inventory = []
 
     def update_room(self, room):
         self.current_room = room
 
-    # def player_name(self, name):
-    #     self.name = name
+    def get_inventory(self):
+        print('\nIventory: ')
+        for item in self.inventory:
+            print(f'{item.name} -- {item.description}')
+    ##take item and drop item here
